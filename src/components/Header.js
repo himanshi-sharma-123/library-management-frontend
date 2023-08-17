e77ec0b98bacafbf19486210fbdaa24a50944bfa
Himanshi Sharma <himanshish456@gmail.com>
2023-08-17 20:37:19 +0530

    changes in ui

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,7 +22,6 @@ const Header = () => {
           >
             <Tab LinkComponent={NavLink} to="/add" label="Add product" />
             <Tab LinkComponent={NavLink} to="/books" label="Books" />
-            {/* <Tab LinkComponent={NavLink} to="/about" label="About Us" /> */}
           </Tabs>
         </Toolbar>
       </AppBar>
