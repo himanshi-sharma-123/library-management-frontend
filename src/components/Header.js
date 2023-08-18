752fe01b092429e6cd40c3a6a08a6ef1e221af60
Himanshi Sharma <himanshish456@gmail.com>
2023-08-18 12:46:09 +0530

    navbar updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { AppBar, Tab, Tabs, Toolbar, Typography } from "@mui/material";
 import LibraryBooksOutlinedIcon from "@mui/icons-material/LibraryBooksOutlined";
 import { NavLink } from "react-router-dom";
 const Header = () => {
-  const [value, setValue] = useState();
+  // const [value, setValue] = useState();
   return (
     <div>
       <AppBar sx={{ backgroundColor: "#232F3D" }} position="sticky">
@@ -13,16 +13,16 @@ const Header = () => {
               <LibraryBooksOutlinedIcon />
             </Typography>
           </NavLink>
-          <Tabs
+          {/* <Tabs
             sx={{ ml: "auto" }}
             textColor="inherit"
             indicatorColor="primary"
             value={value}
             onChange={(e, val) => setValue(val)}
-          >
-            <Tab LinkComponent={NavLink} to="/add" label="Add product" />
-            <Tab LinkComponent={NavLink} to="/books" label="Books" />
-          </Tabs>
+          > */}
+          <Tab LinkComponent={NavLink} to="/add" label="Add product" />
+          <Tab LinkComponent={NavLink} to="/books" label="Books" />
+          {/* </Tabs> */}
         </Toolbar>
       </AppBar>
     </div>
